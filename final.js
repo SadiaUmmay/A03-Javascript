9578977f7ce25e409dfadd1ab1806fb4a1537ab7
unknown <ummayjannatsadia@gmail.com>
2025-08-04 01:26:10 +0600

    final

--- final.js
+++ final.js
@@ -1,5 +1,5 @@
 /** Problem -01 ( Divide the Asset ) */
-var area = 15;
+var area = 800;
 //write your code here
 var perPersonArea = area / 2;
 console.log(perPersonArea)
@@ -19,6 +19,19 @@ else{
 }
 
 
+/** Problem -03 ( Medicine Planner ) */
+var lastDay = 4 ;
+//write your code here
+for (var i=1 ; i<=lastDay; i++){
+   if(i % 3 === 0) {
+    console.log(i+'-medicine')
+   }
+   else{
+    console.log(i+'-rest')
+   }
+}
+
+
 /** Problem 04 - (Delete / Store) */
 var fileName= "pdfData.jpg";
 //write your code here
@@ -28,3 +41,21 @@ if(fileName.startsWith('#') || fileName.endsWith('.pdf') || fileName.endsWith('.
 else{
     console.log('Delete')
 }
+
+
+/** Problem 05 - ( PH Email Generator )  */
+var student= { name: "jhankar" , roll: 1014 ,department: "cse" };
+//write your code here
+var email = (student.name+student.roll+"."+student.department+"@ph.ac.bd")
+console.log(email)
+
+
+/** Problem 06 :  (Current Salary )  */
+var experience = 30;
+var startingSalary = 45000;
+//write your code here
+for(var i= 1 ; i <=experience ; i++){
+    
+    startingSalary +=startingSalary*0.05;
+}
+console.log(startingSalary.toFixed(2));
